a560fdc41b767c405bf5a02a0dfbd3930fcb5602
Minakrv <mnkrv@yahoo.com>
2021-06-16 12:05:11 +0100

    day2

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+const getPerson = require("./lib/person");
+const getPeople = require('./lib/people');
+const path = require ('path');
+const { dirname } = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  
+}));
+
+app.set('view engine', '.hbs');
+
+app.get("/", (req, res) => {
+  let people = getPeople();
+  console.log(people);
+  res.render('index', {people, listExists: true});
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render('about');
+});
+
+app.get("*", (req, res) => {
+  res.status(404).render('404');
+})
+
+app.listen(3000, () => {
+  console.log("Listening to port 3000");
+});
